a2512d28b594539f20cfacbf32d72e6ffc7e66f8
Dungna89 <dungna29@fpt.edu.vn>
2022-07-19 18:29:47 +0700

    1

---
+++ 8_IT17327_NET103_BL2_SM22/BAI_1_3_EFCORE_DBFIRST/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using BAI_1_3_EFCORE_DBFIRST.Context;
+using BAI_1_3_EFCORE_DBFIRST.DomainClass;
+
+namespace BAI_1_3_EFCORE_DBFIRST
+{
+    internal class Program
+    {
+        //Scaffold-DbContext 'Data Source=DUNGNAPC\SQLEXPRESS;Initial Catalog=FINALASS_FPOLYSHOP_SM22_BL2_DUNGNA29;Persist Security Info=True;User ID=dungna34;Password=123456' Microsoft.EntityFrameworkCore.SqlServer -OutputDir DomainClass -context FpolyDBContext -Contextdir Context -DataAnnotations -Force
+        private static void Main(string[] args)
+        {
+            FpolyDBContext dbContext = new FpolyDBContext();
+
+            Nsx nsx = new Nsx() { Id = Guid.Empty, Ma = "IP1", Ten = "Apple1" };
+            dbContext.Nsxes.Add(nsx);
+            dbContext.SaveChanges();
+
+            foreach (var x in dbContext.Nsxes.ToList().Where(c => c.Ma.StartsWith("1")))
+            {
+                Console.WriteLine($"{x.Id} {x.Ma} {x.Ten} ");
+            }
+        }
+    }
+}
